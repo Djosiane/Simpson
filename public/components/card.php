074916dd7cfd262707f70c6ff136667c95b27bb8
nadaissa <nada-issa_student2023@wilder.school>
2023-02-18 13:02:35 +0100

    [HTML] added several cards in the presentation section

--- public/components/card.php
+++ public/components/card.php
@@ -2,8 +2,41 @@
     <div class="card">
         <img class="card__image" src="#" alt="Photo membre d'équipe">
         <div class="card__label">
-            <h2 class="card__h2">Nom</h2>
+            <h2 class="card__h2">Nom 1</h2>
             <p class="card__p">Description du personnage</p>
         </div>
     </div>
+
+    <div class="card">
+        <img class="card__image" src="#" alt="Photo membre d'équipe">
+        <div class="card__label">
+            <h2 class="card__h2">Nom 2</h2>
+            <p class="card__p">Description du personnage</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <img class="card__image" src="#" alt="Photo membre d'équipe">
+        <div class="card__label">
+            <h2 class="card__h2">Nom 3</h2>
+            <p class="card__p">Description du personnage</p>
+        </div>
+    </div>
+    
+    <div class="card">
+        <img class="card__image" src="#" alt="Photo membre d'équipe">
+        <div class="card__label">
+            <h2 class="card__h2">Nom 4</h2>
+            <p class="card__p">Description du personnage</p>
+        </div>
+    </div>
+
+    <div class="card">
+        <img class="card__image" src="#" alt="Photo membre d'équipe">
+        <div class="card__label">
+            <h2 class="card__h2">Nom 5</h2>
+            <p class="card__p">Description du personnage</p>
+        </div>
+    </div>
+
 </div>
