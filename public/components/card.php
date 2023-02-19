a888497eed184be227118a76e50c693dc9beca68
talahik <talahiknaima@gmail.com>
2023-02-19 22:11:23 +0100

    [CSS] modifications of main in index.css

--- public/components/card.php
+++ public/components/card.php
@@ -1,18 +1,21 @@
 <div class="cards-container"> <!--specifique page presentations -->
     <div class="card">
-        <img class="card__image" src="#" alt="Photo membre d'équipe">
+        <img class="card__image" src="/images/image1.jpeg" alt="Photo membre d'équipe">
         <div class="card__label">
             <h2 class="card__h2">Nom 1</h2>
-            <p class="card__p">Description du personnage</p>
+            <p class="card__p">Description du personnage Le module CSS Grid layout est un outil puissant permettant de faire de la mise en page via une grille à deux dimensions.
+Dans cette quête, tu vas découvrir comment utiliser cette technique plutôt récente dans la spécification CSS</p>
+
         </div>
     </div>
 
     <div class="card">
-        <img class="card__image" src="#" alt="Photo membre d'équipe">
         <div class="card__label">
             <h2 class="card__h2">Nom 2</h2>
             <p class="card__p">Description du personnage</p>
         </div>
+         <img class="card__image" src="/images/image1.jpeg" alt="Photo membre d'équipe">
+
     </div>
 
     <div class="card">
